2966dd1366cd0b068207d8c39a6119df94278779
mikke555 <mihhail.kapustin@gmail.com>
2026-08-09 15:04:28 +0300

    use dynamic dates in test to avoid date_from-cannot-be-in-the-past pydantic check

--- tests/const.py
+++ tests/const.py
@@ -1,5 +1,9 @@
-DATE_FROM = "2026-09-01"
-DATE_TO = "2026-09-05"
+from datetime import timedelta
+
+from app.utils.clock import today
+
+DATE_FROM = str(today() + timedelta(days=30))
+DATE_TO = str(today() + timedelta(days=34))
 
 PASSWORD = "password123"
 PASSWORD_HASH = "$argon2id$v=19$m=65536,t=3,p=4$5EA2LBGgz4ZnUJav8JcmeA$RyOCu4XpSCph+/TGxhsw54t1/d3fTvjVtHrorbSRDmU"
